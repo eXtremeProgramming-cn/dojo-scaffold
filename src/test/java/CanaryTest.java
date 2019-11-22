7863c36add65d7bbc108cdac3f810a472428f7b2
Jeff Xiong <gigix1980@gmail.com>
2019-11-22 06:43:02 -0300

    remove dependency to guava

--- src/test/java/CanaryTest.java
+++ src/test/java/CanaryTest.java
@@ -1,12 +1,13 @@
-import com.google.common.io.Files;
 import org.junit.Test;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 
-import static com.google.common.base.Charsets.UTF_8;
-import static com.google.common.collect.Lists.newArrayList;
+import static java.lang.String.join;
+import static java.nio.file.Files.readAllLines;
+import static java.nio.file.Paths.get;
+import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -19,11 +20,11 @@ public class CanaryTest {
 
     @Test
     public void should_be_able_to_use_guava_capabilities() throws IOException {
-        List<Integer> numbers = newArrayList(1, 2, 3, 4, 5);
+        List<Integer> numbers = asList(1, 2, 3, 4, 5);
         List<Integer> doubled = numbers.stream().map(x -> x * 2).collect(toList());
-        assertThat(doubled, is(newArrayList(2, 4, 6, 8, 10)));
+        assertThat(doubled, is(asList(2, 4, 6, 8, 10)));
 
-        String src = Files.toString(new File("src/test/java/CanaryTest.java"), UTF_8);
-        assertThat(src.length(), is(954));
+        String license = join("\n", readAllLines(get("LICENSE")));
+        assertThat(license.length(), is(11356));
     }
 }
