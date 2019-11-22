9357d3e626f04b11f3bcb71b0fb22f8137483371
Jeff Xiong <gigix1980@gmail.com>
2019-11-22 07:00:26 -0300

    remove dependency to guava

--- src/test/java/com/gildedrose/GildedRoseSafetyNet.java
+++ src/test/java/com/gildedrose/GildedRoseSafetyNet.java
@@ -1,20 +1,19 @@
 package com.gildedrose;
 
-import com.google.common.io.Files;
 import org.junit.Test;
 
-import java.io.File;
 import java.io.IOException;
 
 import static com.gildedrose.TextTestFixture.generateBaselineOutput;
-import static com.google.common.base.Charsets.UTF_8;
+import static java.nio.file.Files.readAllBytes;
+import static java.nio.file.Paths.get;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class GildedRoseSafetyNet {
     @Test
     public void should_always_generate_output_identical_to_baseline() throws IOException {
-        String baseline = Files.toString(new File("src/test/baseline.txt"), UTF_8);
+        String baseline = new String(readAllBytes(get("src/test/baseline.txt")));
         assertThat(generateBaselineOutput(), is(baseline));
     }
 }
